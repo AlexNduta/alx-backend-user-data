e13ca6a23e1089977dbf05f83d385bc83a0b671b
was <was@localhost.localdomain>
2024-04-11 20:36:27 +0300

    fixing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,7 @@ import re
 def filter_datum(fields: List[str], redaction: str, message: str,
                  seperator: str) -> str:
     """ create a hash message using regex """
-    pattern = r"(?:{seperator})({'|'.join(fields)})(?={seperator)"
-    return re.sub(pattern, rf"\1={redaction}", message)
+    for field in fields:
+        pattern = f"{field}=[^{seperator}]*"
+        mess = re.sub(pattern, rf"{field}={redaction}", message)
+        return mess
