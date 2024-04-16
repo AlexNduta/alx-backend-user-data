d3d7e23b2da93fa5b14283e74f043be0a961278c
was <was@localhost.localdomain>
2024-04-16 21:36:33 +0300

    fixing

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """ doc doc """
 from flask import request
+import typing
+
 
 class Auth:
     """Authentication class """
-    def require_auth(self, path: str, excluded_paths: List[str]) ->bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns false"""
         pass
 
-    def authorization_header(self, request=None) ->str:
+    def authorization_header(self, request=None) -> str:
         """returns None"""
         requests = request.arg.get()
         return None
 
-    def current_user(self, requests=None) ->TypeVar('User'):
+    def current_user(self, requests=None) -> TypeVar('User'):
         """returns None"""
         requests = request.arg.get()
         return None
