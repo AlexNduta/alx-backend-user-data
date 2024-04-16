1feded2621c445a4634ffc9b695866290a140739
was <was@localhost.localdomain>
2024-04-16 21:33:22 +0300

    fixing

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" doc doc """
+from flask import request
+
+class Auth:
+    """Authentication class """
+    def require_auth(self, path: str, excluded_paths: List[str]) ->bool:
+        """ returns false"""
+        pass
+
+    def authorization_header(self, request=None) ->str:
+        """returns None"""
+        requests = request.arg.get()
+        return None
+
+    def current_user(self, requests=None) ->TypeVar('User'):
+        """returns None"""
+        requests = request.arg.get()
+        return None
