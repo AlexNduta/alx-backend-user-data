c00bb164d9abdee20001c1761689880129db8821
was <was@localhost.localdomain>
2024-04-17 03:58:44 +0300

    fixing

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,13 @@ class Auth:
     """Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns false"""
+        length = len(excluded_paths)
+        for x in excluded_paths:
+            if length is 0:
+                return True
+            else if path is in excluded_paths:
+                return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
