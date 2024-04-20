3f2a14c7187cd862539a60322d43d0962c3b6bb7
was <was@localhost.localdomain>
2024-04-20 21:40:39 +0300

    colon

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,24 +43,24 @@ class BasicAuth(Auth):
             # the exception handled(invalid base 64)
             return None
 
-        def extract_user_credentials(
-                self, decoded_base64_authorization_header: str) -> (str, str):
-            """ extracts the user credentials from the decodes dtring 
-            Return: two strings (user email and password ) seperated by a colon
-            """
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ extracts the user credentials from the decodes dtring 
+        Return: two strings (user email and password ) seperated by a colon
+        """
 
-            if decoded_base64_authorization_header is None:
-                return None, None
+        if decoded_base64_authorization_header is None:
+            return None, None
 
-            if not isinstance(decoded_base64_authorization_header, str):
-                return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
 
-            # if the string do not contain the colon
-            colon = ":"
-            if colon not in decoded_base64_authorization_header:
-                return None, None
+        # if the string do not contain the colon
+        colon = ":"
+        if colon not in decoded_base64_authorization_header:
+            return None, None
 
-            # split the decoded string at the colon point
-            email_pass = decoded_base64_authorization_header.split(':')
-            return email_pass[0], email_pass[1]
+        # split the decoded string at the colon point
+        email_pass = decoded_base64_authorization_header.split(':')
+        return email_pass[0], email_pass[1]
 
