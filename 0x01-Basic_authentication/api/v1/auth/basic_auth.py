4bbcfd25499618a576968bd8cf5b1c889850bb89
was <was@localhost.localdomain>
2024-04-20 19:10:46 +0300

    returnong

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,19 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """ empty for now"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extracts the Base64 auth header
+        -authorization_header: 'Authorisation' key in header
+        """
+
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        starter = authorization_header.startswith('Basic ')
+        if starter is False:
+            return None
+
+        after_basic = authorization_header.split("Basic ")
+        return after_basic
