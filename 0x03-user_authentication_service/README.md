af76d5e8c33c49393eef4539e30ca9359b955750
was <was@localhost.localdomain>
2024-04-24 02:40:36 +0300

    creating

--- 0x03-user_authentication_service/README.md
+++ 0x03-user_authentication_service/README.md
@@ -1,1 +1,8 @@
-# alx-backend-user-data
+# User Authentication service
+
+# Learning Objectives
+
+How to declare API routes in a Flask app
+How to get and set cookies
+How to retrieve request form data
+How to return various HTTP status codes
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
-""" doc doc doc """
-
+"""model `Users` for table `users`
+Columns
+id- integer primary key
+email: a non-nullable string
+hashed_password : a non-nullable string
+session_id : nullable string
+reset_token : nullable string
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """doc doc doc"""
-
-    __tablename__ = "users"
-
+    """ represents our database """
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    email = Column(String)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String)
+    reset_token = Column(String)
